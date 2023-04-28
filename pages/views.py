e45300d73dfc2b67d224dee96c819cad27c0a518
dim <dim@gamil.com>
2023-04-28 19:40:10 +0500

    added templates

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
+
 # Create your views here.
 
 
 def home_view(request):
-    return HttpResponse("Hello world")
+    return render(request, "pages/index.html")
+
+
+def about_view(request):
+    return render(request, "pages/about.html")
