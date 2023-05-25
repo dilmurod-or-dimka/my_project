6797a31a66212eb539a05997e12f5580437d1a56
dim <dim@gamil.com>
2023-05-25 10:02:21 +0500

    fix static-root

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = [
     "dilmurod1506.pythonanywhere.com",
     "127.0.0.1",
 ]
-STATIC_ROOT = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Application definition
